58ca1a7acf643836c49785da4b35a3e5d591ae5e
Papa Daco <papadaco@PapaDaco.local>
2024-11-08 09:53:52 -0500

    PostgreSQL SSL issue; stopping @ 22:00

---
+++ routes/db/postgresql/_middleware.dart
@@ -0,0 +1,24 @@
+import 'package:dart_frog/dart_frog.dart';
+import 'package:postgres/postgres.dart';
+
+Handler middleware(Handler handler) {
+  return (context) async {
+    final connection = await Connection.open(
+      Endpoint(
+        host: 'localhost',
+        port: 5431,
+        database: 'mytasklists',
+        username: 'postgres',
+        password: 'postgres',
+      ),
+    );
+
+    final response = await handler
+        .use(provider<Connection>((_) => connection))
+        .call(context);
+
+    await connection.close();
+
+    return response;
+  };
+}
